147c621d37d5da49275aeaaa598a94d317f72697
Jacob Buchanan <jbuchanan21@qub.ac.uk>
2024-05-11 22:20:30 +0100

    Highlight top-facing side

--- src/scene/meshes/SpinningCube.tsx
+++ src/scene/meshes/SpinningCube.tsx
@@ -1,7 +1,9 @@
 import { useKeyboardControls } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { RapierRigidBody, RigidBody } from "@react-three/rapier";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useMemo, useRef } from "react";
+import { BoxGeometry, Mesh } from "three";
+import * as THREE from "three";
 
 const isStopped = (rapierBody: RapierRigidBody, threshold: number) => {
   const velocity = rapierBody.linvel();
@@ -19,6 +21,29 @@ const isStopped = (rapierBody: RapierRigidBody, threshold: number) => {
 
 const SpinningCube = () => {
   const rapierBody = useRef<RapierRigidBody>(null!);
+  // const geometry = useMemo(() => new BoxGeometry(3, 3, 3), []).toNonIndexed();
+  const geometry = useMemo(
+    () => new THREE.OctahedronGeometry(3),
+    []
+  ).toNonIndexed();
+  const meshRef = useRef<Mesh>(null!);
+
+  geometry.clearGroups();
+
+  let faces = geometry.attributes.position.count / 3;
+  let groupStart = 0;
+  for (let i = 0; i < faces; i++) {
+    geometry.addGroup(groupStart, 3, i);
+    groupStart += 3;
+  }
+
+  const material = useMemo(
+    () =>
+      Array(faces)
+        .fill(0)
+        .map(() => new THREE.MeshStandardMaterial({ color: "blue" })),
+    [faces]
+  );
 
   const fling = useKeyboardControls((state) => {
     return state.fling;
@@ -28,7 +53,48 @@ const SpinningCube = () => {
     if (!rapierBody.current) return;
 
     if (isStopped(rapierBody.current, 0.01)) {
-      console.log(rapierBody.current.rotation());
+      const worldPositions =
+        meshRef.current.geometry.attributes.position.clone();
+
+      worldPositions.applyMatrix4(meshRef.current.matrixWorld);
+
+      // go through each group and check the position of the centre
+      const facePositions = meshRef.current.geometry.groups.map((group) => {
+        const groupVertices = worldPositions.array.slice(
+          group.start * 3,
+          (group.start + group.count) * 3
+        );
+
+        const avg = new THREE.Vector3();
+
+        for (let i = 0; i < groupVertices.length; i += 3) {
+          avg.add(
+            new THREE.Vector3(
+              groupVertices[i],
+              groupVertices[i + 1],
+              groupVertices[i + 2]
+            )
+          );
+        }
+
+        avg.divideScalar(group.count);
+
+        return avg;
+      });
+
+      // for the highest position, set the material to red
+      let highestIndex = 0;
+      for (let i = 1; i < facePositions.length; i++) {
+        if (facePositions[i].y > facePositions[highestIndex].y) {
+          highestIndex = i;
+        }
+      }
+
+      material[highestIndex].color.set("red");
+    } else {
+      material.forEach((mat) => {
+        mat.color.set("blue");
+      });
     }
   });
 
@@ -42,11 +108,13 @@ const SpinningCube = () => {
   });
 
   return (
-    <RigidBody ref={rapierBody} position-y={2} colliders="cuboid" density={4}>
-      <mesh castShadow>
-        <boxGeometry args={[3, 3, 3]} />
-        <meshStandardMaterial color="blue" />
-      </mesh>
+    <RigidBody ref={rapierBody} position-y={2} colliders="trimesh" density={4}>
+      <mesh
+        castShadow
+        ref={meshRef}
+        geometry={geometry}
+        material={material}
+      ></mesh>
     </RigidBody>
   );
 };
